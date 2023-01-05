90f4d2384afcae0bff4de9790782e498dada956f
Bhanuka Anjana <85387483+Bhanu-2500@users.noreply.github.com>
2023-01-05 08:53:30 +0530

    Where method

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿// Linq
+//This style of programming called functional programming
+
 List<int> numbers = new List<int> { 10, 4, 55, -3, 0, -34, 1, 22, 2, 45, 3, 21 };
 
 var sum = numbers.Sum();
@@ -13,3 +15,15 @@ Console.WriteLine($"Max is {max}");
 Console.WriteLine($"Min is {min}");
 Console.WriteLine($"Avarage is {avg}");
 Console.WriteLine($"Last is {last}");
+
+
+//In This "select" method we can pass a function(Type of annonymus Function)
+//Select method return True or False
+//Using Where we can filter numbers, and it returns that number
+
+
+var evenNumbers = numbers.Where(n/*parameter*/ => n%2== 0/*condition*/);
+foreach(var num in evenNumbers)
+{
+    Console.WriteLine(num);
+}
