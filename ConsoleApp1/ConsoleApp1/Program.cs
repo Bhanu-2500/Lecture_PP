039d3278539f3777c62ccbaacd41c3f45ef1df96
Bhanuka Anjana <85387483+Bhanu-2500@users.noreply.github.com>
2023-01-05 08:43:26 +0530

    Second

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,1 +1,15 @@
 ﻿// Linq
+List<int> numbers = new List<int> { 10, 4, 55, -3, 0, -34, 1, 22, 2, 45, 3, 21 };
+
+var sum = numbers.Sum();
+var max = numbers.Max();
+var min = numbers.Min();
+var avg = numbers.Average();
+var last = numbers.Last();
+
+
+Console.WriteLine($"Sum is {sum}");
+Console.WriteLine($"Max is {max}");
+Console.WriteLine($"Min is {min}");
+Console.WriteLine($"Avarage is {avg}");
+Console.WriteLine($"Last is {last}");
